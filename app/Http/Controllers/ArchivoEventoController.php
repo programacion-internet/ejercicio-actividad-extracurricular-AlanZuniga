457fcfb8e2c44398b0a609b589a50fc35c671018
Alan Zuñiga <alan.zuniga@alumnos.udg.mx>
2025-05-07 21:36:53 -0600

    creacion ArchivoEvento -a

---
+++ app/Http/Controllers/ArchivoEventoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ArchivoEvento;
+use App\Http\Requests\StoreArchivoEventoRequest;
+use App\Http\Requests\UpdateArchivoEventoRequest;
+
+class ArchivoEventoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArchivoEventoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ArchivoEvento $archivoEvento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ArchivoEvento $archivoEvento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArchivoEventoRequest $request, ArchivoEvento $archivoEvento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ArchivoEvento $archivoEvento)
+    {
+        //
+    }
+}
